4e1c28eb1708e1c8a6ce6a83876de75625844179
marioapg <30729351+marioapg@users.noreply.github.com>
2017-09-13 09:58:19 -0400

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php
 // Conectando, seleccionando la base de datos
-$link = mysql_connect('host', 'user', 'db')
+$link = mysql_connect('host', 'user', 'pass')
 or die('No se pudo conectar: ' . mysql_error());
 //echo 'Connected successfully';
-$conexion = mysql_select_db('pbwhlugc_audinordb') or die('No se pudo seleccionar la base de datos');
+$conexion = mysql_select_db('db') or die('No se pudo seleccionar la base de datos');
 ?>
